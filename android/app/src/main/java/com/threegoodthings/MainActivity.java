f750bf54646e0f61bc6a013244aa5d1f7f4e911a
raf723 <raf.ansari@thedistance.co.uk>
2022-05-31 13:01:07 +0100

    Android launch screen [Done]

--- android/app/src/main/java/com/threegoodthings/MainActivity.java
+++ android/app/src/main/java/com/threegoodthings/MainActivity.java
@@ -2,6 +2,7 @@ package com.threegoodthings;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -16,6 +17,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this, true);
     super.onCreate(null);
   }
 }
